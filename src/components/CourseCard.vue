2646eb19eb5c991fd1cb3855ff6e8f4b1f47c3f3
Samuele <miotto.samuele@gmail.com>
2021-12-14 15:11:56 +0100

    featured courses styling

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -2,7 +2,8 @@
     <div class="course-card">
                 <img :src="require('../assets/images/' + course.img)" alt="">
                 <div class="card-data">
-                    <h3>{{course.price}}</h3>
+                    <h3 v-if="course.price !== 0">${{course.price}}<span>.00</span></h3>
+                    <h3 v-else>Free</h3>
                     <h4>{{course.title}}</h4>
                     <span><i class="far fa-file-alt"></i> {{course.lessons}} Lessons</span>
                     <span><i class="far fa-user"></i> {{course.students}} Students</span>
@@ -22,12 +23,15 @@ export default {
 <style lang="scss" scoped>
 .course-card {
             margin: 30px 0;
+            padding: 30px;
             height: 200px;
             width: 50%;
             display: flex;
             align-items: center;
+            transition: all .3s ease;
+            border-radius: 5px;
             img {
-                height: 90%;
+                height: 100%;
                 aspect-ratio: 1;
                 object-fit: cover;
                 border-radius: 100%;
@@ -38,9 +42,25 @@ export default {
                 > * {
                     margin-bottom: 30px;
                 }
-                span {
-                    margin-right: 20px;
+                h3 {
+                    color: #20ad96;
+                    font-size: 2rem;
+                    > span {
+                        font-size: 1.6rem;
+                    }
                 }
+                h4 {
+                    color: #3f3a64;
+                }
+                > span {
+                    margin-right: 20px;
+                    color: #a09eb2;
+                }
+            }
+            &:hover {
+                box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.1);
+                cursor: pointer;
+                transform: scale(1.02);
             }
         }
 </style>
--- src/components/FeaturedCourses.vue
+++ src/components/FeaturedCourses.vue
@@ -27,42 +27,42 @@ export default {
                     title: 'Learning to Write as a Professional Author',
                     lessons: 20,
                     students: 50,
-                    price: '$40.00',
+                    price: 40,
                     img: 'course-02-480x298.jpg'
                 },
                 {
                     title: 'Customer-Centric Info-Tech Strategies',
                     lessons: 24,
                     students: 769,
-                    price: 'Free',
+                    price: 0,
                     img: 'stock-full-hd-03-480x298.jpg'
                 },
                 {
                     title: 'Open Programming Courses for Everyone: Python',
                     lessons: 17,
                     students: 62,
-                    price: '$19.00',
+                    price: 19,
                     img: 'stock-full-hd-04-480x298.jpg'
                 },
                 {
                     title: 'Academic Listening and Note-Taking',
                     lessons: 14,
                     students: 67,
-                    price: '$26.00',
+                    price: 26,
                     img: 'stock-full-hd-06-480x298.jpg'
                 },
                 {
                     title: 'Master jQuery in a Short Period of Time',
                     lessons: 6,
                     students: 51,
-                    price: '$39.00',
+                    price: 39,
                     img: 'course-featured-image-01-480x298.jpg'
                 },
                 {
                     title: 'Instroduction to JavaScript for Beginners',
                     lessons: 14,
                     students: 76,
-                    price: '$59.00',
+                    price: 59,
                     img: 'stock-full-hd-05-480x298.jpg'
                 },
             ]
@@ -76,7 +76,16 @@ export default {
 section {
     background: #f5f7fa;
     margin: 50px 0;
+    padding: 100px 0;
     text-align: center;
+    > span {
+        color: #8c89a2;
+        font-size: 1.3rem;
+    }
+    > h2 {
+        color: #3f3a64;
+        font-size: 3rem;
+    }
     .course-container {
         width: 65%;
         margin: 50px auto;
@@ -84,6 +93,21 @@ section {
         flex-wrap: wrap;
         justify-content: space-between;        
     }
+    button {
+            margin-bottom: 35px;
+            padding: 20px 40px;
+            border: none;
+            border-radius: 5px;
+            background: #20ad96;
+            color: #ffffff;
+            font-weight: 500;
+            font-size: 1rem;
+            cursor: pointer;
+            transition: all .2s ease;
+            &:hover {
+                background: #3f3a64;
+            }
+        }
 }
 
 </style>
